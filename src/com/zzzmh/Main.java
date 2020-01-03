6e678fee961437d9506f2a034df849ac2c5545cf
tczmh <admin@zzzmh.cn>
2020-01-03 15:43:58 +0800

    优化逻辑，加入注释

--- src/com/zzzmh/Main.java
+++ src/com/zzzmh/Main.java
@@ -1,5 +1,6 @@
 package com.zzzmh;
 
+import com.zzzmh.controller.IndexController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,13 +11,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/index.fxml"));
-        primaryStage.setTitle("基贱牛难器 (Simple Browser)");
+        // fxml加载器
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("view/index.fxml"));
+        // fxml加载到 根节点
+        Parent root = fxmlLoader.load();
+        // 设置标题
+        primaryStage.setTitle("极简牛腩器 (Simple Browser)");
+        // 设置默认宽高 并加入根节点
         primaryStage.setScene(new Scene(root, 1280, 720));
+        // 显示窗口
         primaryStage.show();
+        // 获取IndexController
+        IndexController controller = (IndexController)fxmlLoader.getController();
+        // 执行IndexController中的初始化方法
+        controller.init();
     }
 
-
+    /**
+     * 入口方法
+     * 执行本方法启动整个程序
+     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/com/zzzmh/controller/IndexController.java
+++ src/com/zzzmh/controller/IndexController.java
@@ -1,31 +1,70 @@
 package com.zzzmh.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.scene.control.TextField;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebErrorEvent;
 import javafx.scene.web.WebView;
 
+/**
+ * @author zzzmh
+ */
 public class IndexController {
 
     public TextField url;
     public WebView webview;
-    public static String homePageUrl = "https://www.bing.com";
+    public static String homePageUrl = "https://www.baidu.com";
 
+
+    /**
+     * 初始化
+     */
+    public void init() {
+        // webview 加入监听器 状态改变后 自动把新的url回显到文本输入框
+        webview.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+            @Override
+            public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
+                url.setText(webview.getEngine().getLocation());
+            }
+        });
+        // 跳转首页
+        home();
+    }
+
+    /**
+     * 后退
+     */
     public void prev() {
-        webview.getEngine().getHistory().go(-1);
-        url.setText(webview.getEngine().getLocation());
+        // 必须不是第一个页 才可以后退 否则会出现
+        if (webview.getEngine().getHistory().getCurrentIndex() > 0) {
+            webview.getEngine().getHistory().go(-1);
+        }
     }
 
+    /**
+     * 前进
+     */
     public void next() {
-        webview.getEngine().getHistory().go(1);
-        url.setText(webview.getEngine().getLocation());
+        // 前进必须前面还有页面
+        if(webview.getEngine().getHistory().currentIndexProperty().getValue()
+                < webview.getEngine().getHistory().getEntries().size() - 1){
+            webview.getEngine().getHistory().go(1);
+        }
     }
 
+    /**
+     * 刷新
+     */
     public void refresh() {
+        // 重新加载当前引擎
         webview.getEngine().reload();
     }
 
+    /**
+     * 首页
+     */
     public void home() {
+        // 跳转默认首页
         goTo(homePageUrl);
     }
 
@@ -34,11 +73,15 @@ public class IndexController {
      * 跳转
      */
     public void forward() {
+        // 跳转到输入的url （本方法关联跳转按钮）
         goTo(url.getText().startsWith("http") ? url.getText() : "http://" + url.getText());
     }
 
+    /**
+     * 跳转的通用方法
+     */
     public void goTo(String urlString) {
-        url.setText(urlString);
         webview.getEngine().load(urlString);
     }
+
 }
